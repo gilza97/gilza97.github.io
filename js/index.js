abbaa3d4eb4487bf35945d83996d89a9da7e4a29
gilza97 <119483584+gilza97@users.noreply.github.com>
2022-12-16 13:48:38 +0500

    neeww fils

--- js/index.js
+++ js/index.js
@@ -1,84 +1,25 @@
-/*burger*/
-let burger = document.querySelector('.burger');
-let meny = document.querySelector('.header__nav');
-let menyLinks = meny.querySelectorAll('.nav__link-main');
+/*map*/
+type = "text/javascript" >
+  ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [55.143054569569955,61.4271224999999],
+    zoom: 17
+  });
 
-burger.addEventListener('click', function () {
-  burger.classList.toggle('burger--active');
-  meny.classList.toggle('header__nav--visible');
-  document.body.classList.toggle('stop-scroll');
-})
+  MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+    '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+  ),
 
-menyLinks.forEach(function (el) {
-  el.addEventListener('click', function () {
-    burger.classList.remove('burger--active');
-    meny.classList.remove('header__nav--visible');
-    document.body.classList.remove('stop-scroll');
-  })
-})
-
-/*search*/
-let search = document.querySelector('.search');
-let searchbox = document.querySelector('.search-ok');
-let search2 = document.querySelector('.search-ok-bthn');
-let search3 = document.querySelector('.search-ok-close');
-
-search.addEventListener('click', function () {
-  searchbox.classList.toggle('search-ok--active');
-  document.body.classList.toggle('stop-scroll');
-})
-
-search2.addEventListener('click', function () {
-  searchbox.classList.remove('search-ok--active');
-  document.body.classList.remove('stop-scroll');
-})
-
-search3.addEventListener('click', function () {
-  searchbox.classList.remove('search-ok--active');
-  document.body.classList.remove('stop-scroll');
-})
-
-/*work*/
-
-let tabsBtn = document.querySelectorAll('.work__bth');
-let tabsItem = document.querySelectorAll('.work__container');
-
-tabsBtn.forEach(function (element) {
-  element.addEventListener('click', function (e) {
-    const path = e.currentTarget.dataset.path;
-
-    tabsBtn.forEach(function (btn) {
-      btn.classList.remove('work__bth--active')
-      e.currentTarget.classList.add('work__bth--active');
+    myPlacemarkWithContent = new ymaps.Placemark([55.143054569569955,61.4271224999999], {
+    }, {
+      iconLayout: 'default#imageWithContent',
+      iconImageHref: 'img/Subtract.svg',
+      iconImageSize: [48, 48],
+      iconImageOffset: [-24, -24],
+      iconContentOffset: [15, 15],
+      iconContentLayout: MyIconContentLayout
     });
 
-    tabsItem.forEach(function (elem) {
-      elem.classList.remove('work__container--active')
-    });
-    document.querySelector(`[data-target="${path}"]`).classList.add('work__container--active');
-  });
-});
-
-/*advertising*/
-
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 1,
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true,
-  },
-});
-
-/*accordion*/
-document.addEventListener("DOMContentLoaded", function () {
-  let acc = new Accordion('.accordion__list', {
-    duration: 700,
-    elementClass: 'accordion__item',
-    triggerClass: 'accordion__top',
-    panelClass: 'accordion__bottom',
-    showMultiple: false,
-    collapse: false
-  });
-})
+  myMap.geoObjects.add(myPlacemarkWithContent);
+};
